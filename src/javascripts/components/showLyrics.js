324730b989420111298a216dc0f4715047892348
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-25 18:41:56 -0500

    added styling and updated README

--- src/javascripts/components/showLyrics.js
+++ src/javascripts/components/showLyrics.js
@@ -1,8 +1,8 @@
 const showLyrics = (songObject, lyrics) => {
   document.querySelector('#lyrics-container').innerHTML = `
-    <h1>${songObject.artist}</h1>
-    <h2>${songObject.title}</h2>`;
-  document.querySelector('#lyrics-container').innerHTML += `<div>${lyrics.lyrics}</div>`;
+  <h1>${songObject.title.toUpperCase()}</h1>
+  <h3 class="mb-4">BY ${songObject.artist.toUpperCase()}</h3>`;
+  document.querySelector('#lyrics-container').innerHTML += `<div id="song">${lyrics.lyrics}</div>`;
 };
 
 export default showLyrics;
